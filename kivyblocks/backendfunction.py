804c0357b17e400cff21a64e1268334a0046f1b1
yumoqing <yumoqing@gmail.com>
2021-01-31 14:41:10 +0800

    bugfix

---
+++ kivyblocks/backendfunction.py
@@ -0,0 +1,6 @@
+from kivy.event import EventDispatcher
+from appPublic.background import Background
+
+class BackendFunction(EventDispatcher, Background):
+	def __init__(self, func, *args, **kw):
+		self.fu
