ebd0e3b0e0d1b799717ce5d61e4c0a16afc029f5
yumoqing <yumoqing@gmail.com>
2022-06-23 19:11:57 +0800

    bugfix

--- kivyblocks/version.py
+++ kivyblocks/version.py
@@ -1,3 +1,6 @@
+# __version__ = '0.2.8'
 # add a new class DefaultImage, if the source load failed, source set to default_source
 __version__ = '0.2.9'
-# __version__ = '0.2.8'
+
+# fix bug in ffpyplayer_video.py
+__version__ = '0.2.10'
