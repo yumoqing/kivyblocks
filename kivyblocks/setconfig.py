37087c30db5f7ce8ec65f85407d31d97559cce37
yumoqing <yumoqing@gmail.com>
2022-07-04 19:59:38 +0800

    bugif

---
+++ kivyblocks/setconfig.py
@@ -0,0 +1,10 @@
+import kivy
+import kivyblocks
+from kivy.config import Config
+from kivy.resources import resource_add_path
+resource_add_path(os.path.join(os.path.dirname(kivyblocks.__file__),'./ttf'))
+Config.set('kivy', 'default_font', [
+        'msgothic',
+        'DroidSansFallback.ttf'])
+
+config_set = True
