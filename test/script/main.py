f8c0dbedc058231a812509892dc9e5348c1c9a4e
yumoqing <yumoqing@gmail.com>
2022-08-25 21:00:09 +0800

    add script test

---
+++ test/script/main.py
@@ -0,0 +1,10 @@
+from kivyblocks.blocksapp import BlocksApp
+from kivyblocks.blocks import registerWidget, Blocks
+import kivyblocks.register
+
+
+class ScriptApp(BlocksApp):
+	pass
+
+if __name__ == '__main__':
+	ScriptApp().run()
